8bb6b7882f32efa595e384bfb9e996f39b05a107
MaximMukhin <eriwer@gmail.com>
2022-04-14 23:38:07 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+
 
 function App() {
+
+  const [value, setValue] = useState('text')
+
+
+
   return (
     <div className="App">
+      <Counter />
+      <hr />
  
-    
-      
+      <ClassCounter/>
+
+      <hr />
+      <h1>{value}</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      />
+
+
     </div>
   );
 }
