8cb11d46d997f6d7bef23045781584470a5646fa
MaximMukhin <eriwer@gmail.com>
2022-04-17 21:59:27 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
 import ImputText from "./components/ImputText";
-/* import PeopleList from "./components/PeopleList"; */
+import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
-/* import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput"; */
+import MyInput from "./components/UI/input/MyInput";
+import MySelect from "./components/UI/select/MySelect";
+/* import PeopleList from "./components/PeopleList"; */
 import './styles/App.css'
 
 function App() {
 
-   const addNewPost = (e) => {
-      e.preventDefault()
-      setPosts([...posts, {...post, id: Date.now()}])
-      setPost({title: '', body: ''})
 
-   }
 
    const [posts, setPosts] = useState([
-      { id: 1, title: 'JavaScript 1', body: 'JS Description 1' },
-      { id: 2, title: 'JavaScript 2', body: 'JS Description 2' },
-      { id: 3, title: 'JavaScript 3', body: 'JS Description 3' },
+      { id: 1, title: 'dd JavaScript 1', body: '32 JS Description 1' },
+      { id: 2, title: 'zz JavaScript 2', body: '1  JS Description 2' },
+      { id: 3, title: '2 JavaScript 3', body: '9   JS Description 3' },
    ])
+
+   const [selectedSort, setSelectedSort] = useState('')
+
+   const createPost = (newPost) => {
+      console.log(newPost);
+      if (newPost.title !== '' && newPost.body !== '') {
+         setPosts([...posts, newPost])
+      } else {
+         console.log('Добавьте текст');
+      }
+   }
+
+   const removePost = (post) => {
+      setPosts(posts.filter(p => p.id !== post.id))
+      console.log('removePost', post);
+   }
+
 
 /*    const [peoples, setPeoples] = useState([
       { id: 1, name: 'Max', lastName: 'Mukhin', budget: '60000' },
@@ -28,17 +41,42 @@ function App() {
       { id: 3, name: 'Pasha', lastName: 'Ganushkin', budget: '55000' },
    ]) */
 
+   const sortPosts = (sort) => {
+      setSelectedSort(sort)
+      setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+   }
+
   return (
     <div className="App">
       <Counter />
-      <hr />
+        <hr style={{ margin: '15px 0' }} />
         <ImputText />
-        <hr />
-        -------------
+        <hr style={{ margin: '15px 0' }} />
 
-        <hr />
 
-        <PostList posts={posts} title='Посты по JS' />
+
+        <PostForm create={createPost} />
+        <hr style={{ margin: '15px 0' }} />
+        <div>
+           <MyInput
+              placeholder='Поиск'
+           />
+           <MySelect
+              value={selectedSort}
+              onChange={sortPosts}
+              defaultValue={'Сортировка'}
+              options={[
+                 { value: 'title', name: 'По названию' },
+                 { value: 'body', name: 'По описанию' }
+              ]}
+
+           />
+        </div>
+        {posts.length !== 0
+           ? <PostList remove={removePost} posts={posts} title='Посты по JS' />
+           : <h1 style={{ textAlign: 'center' }}>Посты не найдены</h1>
+        }
+
 
 
 {/*         <PeopleList peoples={peoples} /> */}
