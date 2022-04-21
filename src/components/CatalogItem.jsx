c3b76279655edd5ac4bfd5ab7eff659f5ae9077a
MaximMukhin <eriwer@gmail.com>
2022-04-21 21:25:24 +0300

    1

--- src/components/CatalogItem.jsx
+++ src/components/CatalogItem.jsx
@@ -6,12 +6,8 @@ const CatalogItem = (props) => {
   return (
     <div className="post">
       <div className="post__content">
-<<<<<<< HEAD
         <p>index: {props.number} </p>
         <hr style={{ margin: '15px 0' }}/>
-=======
-        <strong>{props.number} </strong>
->>>>>>> de2169d3af2593b9b07847d5d4a27f7380aa8c7c
         <img style={{ maxWidth: '200px' }} src={`/images/${props.product.image}`} alt="картинка" />
         <p>Название: {props.product.name}</p>
         <p>Цвет: {props.product.color}</p>
