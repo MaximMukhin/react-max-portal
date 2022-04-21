de2169d3af2593b9b07847d5d4a27f7380aa8c7c
MaximMukhin <eriwer@gmail.com>
2022-04-21 08:35:10 +0300

    2

--- src/components/CatalogItem.jsx
+++ src/components/CatalogItem.jsx
@@ -7,9 +7,11 @@ const CatalogItem = (props) => {
     <div className="post">
       <div className="post__content">
         <strong>{props.number} </strong>
+        <img style={{ maxWidth: '200px' }} src={`/images/${props.product.image}`} alt="картинка" />
         <p>Название: {props.product.name}</p>
         <p>Цвет: {props.product.color}</p>
-        <p>Цена: {props.product.price}</p>
+        <p>Цена: {props.product.price}руб</p>
+        <p>Склад: {props.product.available}шт</p>
       </div>
       <div className="post__btns">
         <MyButton onClick={() => props.remove(props.product)}>Удалить</MyButton>
